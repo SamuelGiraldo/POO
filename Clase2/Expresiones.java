4b36521e9837608daa5e8be41cb1d1d13e74658c
Samuel Giraldo <samuel.giraldov@campusucc.edu.co>
2025-08-13 22:07:18 +0000

    Ejercicio 3.8 C

--- Clase2/Expresiones.java
+++ Clase2/Expresiones.java
@@ -4,6 +4,6 @@ public class Expresiones {
    int A = 4;
    int B = 5;
    int C = 1;
-    System.out.println( B * A - Math.pow (B,2) / 4 * C );
+    System.out.println((((B + C) /2 * A +10) *3 * B) -6);
     }
 }
