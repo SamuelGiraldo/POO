21a858db0d4a6e8f373865fe37bb454810bd9074
Samuel Giraldo <samuel.giraldov@campusucc.edu.co>
2025-09-17 21:20:23 +0000

    update

---
+++ Clase7/Persona.java
@@ -0,0 +1,16 @@
+package Clase7;
+public class Persona {
+    //Atributos
+    private int identificacion;
+    private String nombres;
+    private String apellidos;
+    private int edad;
+    private double estatura;
+    private double peso;
+
+    //Metodos
+    public void caminar(){}
+    public boolean jugar(){}
+    public void comer(){}
+    public string estudiar(){}
+}
