88314684261be7e54a904fca62f4c02c8c28b99b
Samuel Giraldo <samuel.giraldov@campusucc.edu.co>
2025-11-05 17:45:57 +0000

    Update

---
+++ Trabajo/Main.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Cliente c1 = new Cliente("1105371292", "Samuel Giraldo");
+
+        Foto[] fotos = { new Foto("foto1.jpg"), new Foto("foto2.jpg") };
+        Producto imp = new Impresion(1, "Color", fotos);
+        Producto cam = new Camara(2, "Sony", "(A7 III");
+
+        Producto[] lista = { imp, cam };
+
+        Pedido pedido = new Pedido(c1, lista, new Date(), 20746925);
+        pedido.mostrarPedido();
+    }
+}
+
