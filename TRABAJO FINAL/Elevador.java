b0b76a1a20eafbb95b4d92443d89194bb674505f
Samuel Giraldo <samuel.giraldov@campusucc.edu.co>
2025-11-18 23:17:30 +0000

    update

---
+++ TRABAJO FINAL/Elevador.java
@@ -0,0 +1,74 @@
+public class Elevador {
+
+    private int pisoActual;
+    private boolean puertaAbierta;
+    private final int pisoMin;
+    private final int pisoMax;
+
+    public Elevador(int pisoMin, int pisoMax) {
+        this.pisoMin = pisoMin;
+        this.pisoMax = pisoMax;
+        this.pisoActual = pisoMin;
+        this.puertaAbierta = false;
+    }
+
+    public int getPisoActual() {
+        return pisoActual;
+    }
+
+    public void abrirPuerta() {
+        if (!puertaAbierta) {
+            puertaAbierta = true;
+            System.out.println("Puerta abierta.");
+        } else {
+            System.out.println("La puerta ya está abierta.");
+        }
+    }
+
+    public void cerrarPuerta() {
+        if (puertaAbierta) {
+            puertaAbierta = false;
+            System.out.println("Puerta cerrada.");
+        } else {
+            System.out.println("La puerta ya está cerrada.");
+        }
+    }
+
+    public void irA(int pisoDestino) {
+        if (pisoDestino < pisoMin || pisoDestino > pisoMax) {
+            System.out.println("❌ Piso inválido.");
+            return;
+        }
+
+        if (puertaAbierta) {
+            System.out.println("Cerrando puerta para mover el elevador...");
+            cerrarPuerta();
+        }
+
+        System.out.println("Moviendo elevador...");
+        while (pisoActual != pisoDestino) {
+            if (pisoActual < pisoDestino) {
+                subir();
+            } else {
+                bajar();
+            }
+        }
+
+        System.out.println("Llegaste al piso " + pisoActual + ".");
+        abrirPuerta();
+    }
+
+    private void subir() {
+        if (pisoActual < pisoMax) {
+            pisoActual++;
+            System.out.println("Subiendo... Piso: " + pisoActual);
+        }
+    }
+
+    private void bajar() {
+        if (pisoActual > pisoMin) {
+            pisoActual--;
+            System.out.println("Bajando... Piso: " + pisoActual);
+        }
+    }
+}
---
+++ TRABAJO FINAL/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Elevador elevador = new Elevador(0, 10);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("=== SISTEMA DE ELEVADOR ===");
+
+        while (true) {
+            System.out.println("\nPiso actual: " + elevador.getPisoActual());
+            System.out.println("1. Ir a un piso");
+            System.out.println("2. Abrir puerta");
+            System.out.println("3. Cerrar puerta");
+            System.out.println("4. Salir");
+            System.out.print("Opción: ");
+
+            int opcion = scan.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese piso destino: ");
+                    int destino = scan.nextInt();
+                    elevador.irA(destino);
+                    break;
+
+                case 2:
+                    elevador.abrirPuerta();
+                    break;
+
+                case 3:
+                    elevador.cerrarPuerta();
+                    break;
+
+                case 4:
+                    System.out.println("Saliendo...");
+                    return;
+
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        }
+    }
+}
